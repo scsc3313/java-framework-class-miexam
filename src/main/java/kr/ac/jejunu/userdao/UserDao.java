158c7def5a8f78fc232ea06dd15b37d1db14340a
HSH <scsc3313@naver.com>
2016-04-22 16:47:57 +0900

    Exception 발생

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -11,43 +11,89 @@ public class UserDao {
     }
 
     public User get(Long id) throws ClassNotFoundException, SQLException {
-        //데이터는어디에?   Mysql
-        //Driver Class Load
-        Connection connection = connectionMaker.getConnection();
-        // 쿼리만들고
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
-        preparedStatement.setLong(1, id);
-        // 실행
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        // 결과매핑
-        User user = new User();
-        user.setId(resultSet.getLong("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
+        User user = null;
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+        try {
+            //데이터는어디에?   Mysql
+            //Driver Class Load
+            connection = connectionMaker.getConnection();
+            // 쿼리만들고
+            preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+            preparedStatement.setLong(1, id);
+            // 실행
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            // 결과매핑
+            user = new User();
+            user.setId(resultSet.getLong("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
 
-        //자원을 해지한다.
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        } catch (ClassNotFoundException e) {
+            throw new ClassNotFoundException();
+        } catch (SQLException e) {
+            throw new SQLException();
+        } finally {
+            //자원을 해지한다.
+            if(resultSet != null)
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if(preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if(connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
 
         return user;
     }
 
     public Long add(User user) throws ClassNotFoundException, SQLException {
-        //데이터는어디에?   Mysql
-        //Driver Class Load
-        Connection connection = connectionMaker.getConnection();
-        // 쿼리만들고
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo (name, password) values (?, ?)");
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
-        preparedStatement.executeUpdate();
+        Long id = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            //데이터는어디에?   Mysql
+            //Driver Class Load
+            connection = connectionMaker.getConnection();
+            // 쿼리만들고
+            preparedStatement = connection.prepareStatement("insert into userinfo (name, password) values (?, ?)");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.executeUpdate();
 
-        Long id = getLastInsertId(connection);
+            id = getLastInsertId(connection);
 
-        preparedStatement.close();
-        connection.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if(preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if(connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+        }
 
         return id;
     }
